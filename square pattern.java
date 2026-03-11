816bd0a0985bb7e427593baa8fc9177cfe20972d
Laveen pappu <luckysteven5757@gmail.com>
2026-03-11 14:11:19 +0530

    push

--- square pattern.java
+++ square pattern.java
@@ -1,22 +1,24 @@
 import java.util.*;
-class Main {
+
+public class Solution {
     public static void main(String[] args) {
-        
-        
+    
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the value");
         int a=sc.nextInt();
         System.out.println("enter the symbol");
-        char b=sc.next().charAt(0);
+        char b='*';
         for(int i=1;i<=a;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(b + " ");
+            for(int j=1;j<=a;j++){
+                if(i==1||i==a||j==1||j==a){
+                    System.out.print(b +" ");
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
-       
         
-//outer loop -> rows
-//inner loop->symbol
     }
 }
